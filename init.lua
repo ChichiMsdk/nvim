ea1f4e13ce82a1f1a820ca9a84aad6199c0f2be3
Chihab <chihab.moussadyk@gmail.com>
2024-03-04 13:57:07 +0100

    changes

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("base")
 --vim.g.loaded_netrwPlugin = 1
 -- set clipboard= using luascript
 --vim.cmd("set clipboard=")
+vim.cmd[[lan en]]
+vim.cmd[[tnoremap <Esc> <C-\><C-n>]]
 vim.g.clipboard = {
          name= 'Win32yank',
          copy = { 
